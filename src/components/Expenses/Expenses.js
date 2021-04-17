53778fc3ad9b17ec1a65632db724ba4aef55677d
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-17 22:36:34 +0300

    Expense Filter functionality added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,31 @@
+import React, { useState } from 'react';
+import ExpenseFilter from '../ExpenseFilter/ExpensesFilter';
 import ExpenseItem from '../ExpenseItem/ExpenseItem';
 import './Expenses.css';
 
-const Expenses = ({ expenses, onClick }) => {
+const Expenses = ({ expenses }) => {
+  const [selectedYear, setSelectedYear] = useState('');
+
+  const yearSelectHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+  };
+
+  const filteredExpenses =
+    selectedYear === ''
+      ? expenses
+      : expenses.filter(
+          (expense) => expense.date.getFullYear().toString() === selectedYear
+        );
+
   return (
     <div className='expenses'>
-      {expenses.map((expense) => (
+      <ExpenseFilter selected={selectedYear} onYearSelect={yearSelectHandler} />
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           date={expense.date}
           title={expense.title}
           amount={expense.amount}
-          onClick={() => onClick(expense.id)}
         />
       ))}
     </div>
