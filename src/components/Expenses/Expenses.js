4070b7ab56b46631677be048d50d78748af579cc
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-18 16:12:01 +0300

    ExpensesChart and PropTypes added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,8 @@ import { filterExpenses } from '../../utils/helpers';
 import ExpenseFilter from '../ExpenseFilter/ExpensesFilter';
 import ExpenseList from '../ExpenseList/ExpenseList';
 import './Expenses.css';
+import PropTypes from 'prop-types';
+import ExpensesChart from '../ExpensesChart/ExpensesChart';
 
 const Expenses = ({ expenses }) => {
   const [selectedYear, setSelectedYear] = useState('');
@@ -20,9 +22,14 @@ const Expenses = ({ expenses }) => {
         onYearSelect={yearSelectHandler}
         initYear={2018}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpenseList items={filteredExpenses} year={selectedYear} />
     </div>
   );
+};
+
+Expenses.propTypes = {
+  expenses: PropTypes.array.isRequired,
 };
 
 export default Expenses;
